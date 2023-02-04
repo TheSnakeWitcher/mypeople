66983d85fc66897c587e9ff5147a92fccafe9556
Mr.PAPI <thesnakewitcher@gmail.com>
2023-02-04 15:14:27 -0500

    mod: add cli command show help

--- src/cli/add.rs
+++ src/cli/add.rs
@@ -1,7 +1,8 @@
+use super::args;
 use clap::{Arg, ArgAction, Command};
 
 pub fn new() -> Command {
-    Command::new("add").arg(
-        Arg::new("name").action(ArgAction::Set)
-    )
+    Command::new("add")
+        .arg_required_else_help(true)
+        .arg(args::name())
 }
