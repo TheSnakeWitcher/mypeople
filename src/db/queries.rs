211553ae5e23222c00b16354ab13c32657e59fed
Mr.PAPI <thesnakewitcher@gmail.com>
2023-02-04 17:56:02 -0500

    add: queries module
    to export from here all queries that need to be executed from main

---
+++ src/db/queries.rs
@@ -0,0 +1,5 @@
+pub use super::contacts::{
+    get_all_contacts, get_contact, get_contacts, insert_contact, insert_contacts, remove_contact,
+    remove_contacts,
+};
+pub use super::groups::{get_groups, insert_group, remove_group};
