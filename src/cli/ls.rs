b3b6053d59b42ab90da4df043540d75cd0030ab8
Mr.PAPI <thesnakewitcher@gmail.com>
2023-02-05 13:49:26 -0500

    fix: cli/ls.rs use arg::emails

--- src/cli/ls.rs
+++ src/cli/ls.rs
@@ -1,10 +1,10 @@
 use super::args;
-use clap::{Arg, ArgAction, Command};
+use clap::Command;
 
 pub fn new() -> Command {
     Command::new("ls")
         .arg(args::name())
         .arg(args::groups())
-        .arg(args::email())
+        .arg(args::emails())
         .arg(args::phones())
 }
