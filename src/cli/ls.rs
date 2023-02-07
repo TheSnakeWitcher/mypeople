36b85985d6fedf2304fa70b813943a1e378178c8
Mr.PAPI <thesnakewitcher@gmail.com>
2023-02-06 23:09:09 -0500

    fix: args always present with default value false when action SetTrue seted
    when submatches.args_present() was called cli detect that these args exist with default value false

--- src/cli/ls.rs
+++ src/cli/ls.rs
@@ -4,7 +4,7 @@ use clap::Command;
 pub fn new() -> Command {
     Command::new("ls")
         .arg(args::name())
-        .arg(args::groups())
-        .arg(args::emails())
-        .arg(args::phones())
+        .arg(args::emails().default_missing_value("true"))
+        .arg(args::groups().default_missing_value("true"))
+        .arg(args::phones().default_missing_value("true"))
 }
