a014bfd13b72541870402695db80544e0a338ea0
Mr.PAPI <thesnakewitcher@gmail.com>
2023-02-08 08:07:59 -0500

    add: wallets queries to queries module

--- src/db/queries.rs
+++ src/db/queries.rs
@@ -6,4 +6,5 @@ pub use super::groups::{insert_group, remove_group};
 pub use super::phones::{insert_phone, insert_phones, remove_phone};
 pub use super::emails::{insert_email, insert_emails, remove_email};
 pub use super::social_nets::{insert_social_net, insert_social_nets, remove_social_net};
+pub use super::wallets::{insert_wallet,insert_wallets,remove_wallet} ;
 pub use super::pic::insert_pic;
