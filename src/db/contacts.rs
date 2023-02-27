14af530767b4213f2aca70b1e43f17c343e010d2
Mr.PAPI <thesnakewitcher@gmail.com>
2023-02-27 18:29:44 -0500

    refactor: replace query! macro with query function
    this is due query! macro depends on DATABASE_URL
    environment variable to execute queries

--- src/db/contacts.rs
+++ src/db/contacts.rs
@@ -1,29 +1,26 @@
 use super::schema::Contact;
 use sqlx::{
-    query,
-    sqlite::{SqliteConnection, SqliteQueryResult},
-    Error,
+    query, query_as,
+    sqlite::{SqliteConnection, SqliteQueryResult, SqliteRow},
+    Error, FromRow,
 };
 
 pub async fn get_all_contacts(conn: &mut SqliteConnection) -> Result<Vec<Contact>, Error> {
-    let rows = query!("SELECT * FROM contacts ORDER BY name ;")
-        .map(|item| Contact {
-            id: item.id,
-            name: item.name,
-            pic: item.pic,
-            groups: serde_json::from_str(&item.groups).unwrap(),
-            emails: serde_json::from_str(&item.emails).unwrap_or_default(),
-            phones: serde_json::from_str(&item.phones).unwrap_or_default(),
-            social_nets: serde_json::from_str(&item.phones).unwrap_or_default(),
-            wallets: serde_json::from_str(&item.wallets).unwrap_or_default(),
-            locations: serde_json::from_str(&item.locations).unwrap_or_default(),
-            events: serde_json::from_str(&item.events).unwrap_or_default(),
-            notes: item.notes,
-        })
+    let rows = query("SELECT * FROM contacts ORDER BY name ;")
+        .map(|item: SqliteRow| Contact::from_row(&item))
         .fetch_all(conn)
         .await?;
-
-    Ok(rows)
+    let out = rows
+        .into_iter()
+        .filter_map(|item| {
+            if item.is_ok() {
+                Some(item.unwrap())
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<Contact>>();
+    Ok(out)
 }
 
 pub async fn get_contacts(
@@ -40,20 +37,8 @@ pub async fn get_contacts(
 }
 
 pub async fn get_contact(conn: &mut SqliteConnection, name: &str) -> Result<Contact, Error> {
-    let contact = query!("SELECT * FROM contacts WHERE name = ? LIMIT 1 ;", name)
-        .map(|item| Contact {
-            id: item.id,
-            name: item.name,
-            pic: item.pic,
-            groups: serde_json::from_str(&item.groups).unwrap(),
-            emails: serde_json::from_str(&item.emails).unwrap_or_default(),
-            phones: serde_json::from_str(&item.phones).unwrap_or_default(),
-            social_nets: serde_json::from_str(&item.phones).unwrap_or_default(),
-            wallets: serde_json::from_str(&item.wallets).unwrap_or_default(),
-            locations: serde_json::from_str(&item.locations).unwrap_or_default(),
-            events: serde_json::from_str(&item.events).unwrap_or_default(),
-            notes: item.notes,
-        })
+    let contact = query_as::<_, Contact>("SELECT * FROM contacts WHERE name = ? LIMIT 1 ;")
+        .bind(name)
         .fetch_one(conn)
         .await?;
 
