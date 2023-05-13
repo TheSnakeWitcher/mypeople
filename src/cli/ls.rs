46d2d5fd28a10426a67f6c1276d8b8d4d191f472
Mr.PAPI <thesnakewitcher@gmail.com>
2023-05-12 22:22:14 -0400

    add: filter options to ls cmd

--- src/cli/ls.rs
+++ src/cli/ls.rs
@@ -2,5 +2,12 @@ use super::args;
 use clap::Command;
 
 pub fn new() -> Command {
-    Command::new("ls").arg(args::name())
+    Command::new("ls")
+        .arg(args::name())
+        .arg(args::pic())
+        .arg(args::groups())
+        .arg(args::phones())
+        .arg(args::emails())
+        .arg(args::social_nets())
+        .arg(args::wallets())
 }
