3885cf8b00db120fe863c07eb59b6f0038ead1c3
Mr.PAPI <thesnakewitcher@gmail.com>
2023-05-13 12:35:08 -0400

    ref: cli args module

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -8,78 +8,48 @@ pub fn name() -> Arg {
     Arg::new("name").num_args(0..).action(ArgAction::Set)
 }
 
-pub fn pic() -> Arg {
-    Arg::new("pic").long("pic").action(ArgAction::Set)
+pub fn pic(action: ArgAction) -> Arg {
+    Arg::new("pic").long("pic").action(action)
 }
 
-pub fn groups() -> Arg {
+pub fn groups(action: ArgAction) -> Arg {
     Arg::new("groups")
         .short('g')
         .long("groups")
-        .action(ArgAction::Set)
+        .action(action)
 }
 
-pub fn no_groups() -> Arg {
-    Arg::new("no-groups")
-        .long("no-groups")
-        .action(ArgAction::Set)
-}
-
-pub fn phones() -> Arg {
+pub fn phones(action: ArgAction) -> Arg {
     Arg::new("phones")
         .short('p')
         .long("phones")
-        .action(ArgAction::Set)
+        .action(action)
 }
 
-pub fn no_phones() -> Arg {
-    Arg::new("no-phones")
-        .long("no-phones")
-        .action(ArgAction::Set)
-}
-
-pub fn emails() -> Arg {
+pub fn emails(action: ArgAction) -> Arg {
     Arg::new("emails")
         .short('e')
         .long("emails")
-        .action(ArgAction::Set)
+        .action(action)
 }
 
-pub fn no_emails() -> Arg {
-    Arg::new("no-emails")
-        .long("no-emails")
-        .action(ArgAction::Set)
-}
-
-pub fn social_nets() -> Arg {
+pub fn social_nets(action: ArgAction) -> Arg {
     Arg::new("social_nets")
         .short('s')
         .long("social-nets")
-        .action(ArgAction::Set)
+        .action(action)
 }
 
-pub fn no_social_nets() -> Arg {
-    Arg::new("no-social_nets")
-        .long("no-social-nets")
-        .action(ArgAction::Set)
-}
-
-pub fn wallets() -> Arg {
+pub fn wallets(action: ArgAction) -> Arg {
     Arg::new("wallets")
         .short('w')
         .long("wallets")
-        .action(ArgAction::Set)
+        .action(action)
 }
 
-pub fn no_wallets() -> Arg {
-    Arg::new("no-wallets")
-        .long("no-wallets")
-        .action(ArgAction::Set)
-}
-
-pub fn notes() -> Arg {
+pub fn notes(action: ArgAction) -> Arg {
     Arg::new("notes")
         .short('n')
         .long("notes")
-        .action(ArgAction::Set)
+        .action(action)
 }
