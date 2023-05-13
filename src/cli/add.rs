85424e6016b426a61a9b92ac8203401043f0e8db
Mr.PAPI <thesnakewitcher@gmail.com>
2023-05-13 12:35:41 -0400

    ref: cli add command

--- src/cli/add.rs
+++ src/cli/add.rs
@@ -1,14 +1,14 @@
 use super::args;
-use clap::Command;
+use clap::{ArgAction, Command};
 
 pub fn new() -> Command {
     Command::new("add")
         .arg_required_else_help(true)
         .arg(args::name())
-        .arg(args::pic())
-        .arg(args::groups())
-        .arg(args::phones())
-        .arg(args::emails())
-        .arg(args::social_nets())
-        .arg(args::wallets())
+        .arg(args::pic(ArgAction::Set))
+        .arg(args::groups(ArgAction::Set))
+        .arg(args::phones(ArgAction::Set))
+        .arg(args::emails(ArgAction::Set))
+        .arg(args::social_nets(ArgAction::Set))
+        .arg(args::wallets(ArgAction::Set))
 }
