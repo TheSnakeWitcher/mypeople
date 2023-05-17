e767b48f84bd8817386f104aa47d4c582143b56e
Mr.PAPI <thesnakewitcher@gmail.com>
2023-05-17 15:51:55 -0400

    add: import module to dispatchers module

--- src/dispatchers/mod.rs
+++ src/dispatchers/mod.rs
@@ -2,7 +2,9 @@ mod add;
 mod aux;
 mod ls;
 mod rm;
+mod import;
 
 pub use add::add_cmd_dispatcher;
 pub use ls::ls_cmd_dispatcher;
 pub use rm::rm_cmd_dispatcher;
+pub use import::import_cmd_dispatcher;
