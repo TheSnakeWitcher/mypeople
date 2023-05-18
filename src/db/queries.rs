e307c1a4ffb9636942f1db979d9a40d768b57855
Mr.PAPI <thesnakewitcher@gmail.com>
2023-05-17 20:30:41 -0400

    add: insert_gropus to queries module to make it public

--- src/db/queries.rs
+++ src/db/queries.rs
@@ -2,9 +2,9 @@ pub use super::contacts::{
     get_all_contacts, get_contact, get_contacts, insert_contact, insert_contacts, remove_contact,
     remove_contacts,
 };
-pub use super::groups::{insert_group, remove_group};
-pub use super::phones::{insert_phone, insert_phones, remove_phone};
 pub use super::emails::{insert_email, insert_emails, remove_email};
-pub use super::social_nets::{insert_social_net, insert_social_nets, remove_social_net};
-pub use super::wallets::{insert_wallet,insert_wallets,remove_wallet} ;
+pub use super::groups::{insert_group, insert_groups, remove_group};
+pub use super::phones::{insert_phone, insert_phones, remove_phone};
 pub use super::pic::insert_pic;
+pub use super::social_nets::{insert_social_net, insert_social_nets, remove_social_net};
+pub use super::wallets::{insert_wallet, insert_wallets, remove_wallet};
